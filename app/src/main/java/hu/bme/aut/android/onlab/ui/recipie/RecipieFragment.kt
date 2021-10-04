88d857c121d8df13b5e9dd94eba314b0fe5e0ab1
szkatica22 <szkatica22@gmail.com>
2021-10-04 13:28:26 +0200

    Recipie - Ingredients ListView

--- app/src/main/java/hu/bme/aut/android/onlab/ui/recipie/RecipieFragment.kt
+++ app/src/main/java/hu/bme/aut/android/onlab/ui/recipie/RecipieFragment.kt
@@ -1,9 +1,13 @@
 package hu.bme.aut.android.onlab.ui.recipie
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -31,15 +35,45 @@ class RecipieFragment : Fragment(){
         _binding = FragmentRecipieBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textRecipie
+        val textView: TextView = binding.tvRecipieName
         recipieViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
         // Teszt gomb a kovi fragmentre valtashoz
-        binding.testButton3.setOnClickListener{
+        binding.imgBtnEdit.setOnClickListener{
             findNavController().navigate(R.id.action_nav_recipie_to_nav_change_recipie)
         }
+
+
+        // Recipie Ingredients
+        val array_adapter: ArrayAdapter<*>
+        val ingredients = arrayOf("1 cup espresso", "6 egg yolks", "6 Tbsp rum", "30 ladyfingers",
+            "3/4 cup sugar", "425 g mascarpone", "2 cups heavy whipping cream", "2-3 TBSP cocoa powder")
+
+        var list_view = binding.lvRecipieIngredients
+        array_adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredients)
+        list_view.adapter = array_adapter
+
+
+//        // Recept flag-k a spinner-hez ideiglenes ertekek
+//        val flags = arrayOf("Desserts", "Main courses", "Soups", "Drinks")
+//        val array_adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, flags)
+//
+//        // attached arrayadapter to spinner
+//        binding.spnrRecipieCategory.adapter = array_adapter
+//
+//        binding.spnrRecipieCategory.onItemSelectedListener = object :
+//            AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                binding.tvRecipieSpinnerSelected.text = flags[p2]
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//            }
+//
+//        }
+
 
         return root
     }
